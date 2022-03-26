5cf119e9f31fd493bb67f6477c606d04b1551626
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:08:56 +0300

    sum

---
+++ proje22.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int sayi1,sayi2,toplam;
+	
+	sayi1=24;
+	sayi2=28;
+	toplam=sayi1+sayi2;
+	
+	if(toplam>=50)
+	{
+		printf("Toplam Degeriniz: %d - 50 den buyuk",toplam);
+	}
+	else
+	{
+		printf("Toplam Degeriniz: %d - 50 den kucuk",toplam);
+	}
+	
+	return 0;
+}
